3a7fa85aee58fda3956ab8e9a8129962c67559de
iam-harshit <importantemail545@gmail.com>
2025-01-07 12:36:06 +0530

    feat(App): Add image with CDN link for testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ const App = () => {
     <>
       <div className="flex justify-center items-center">
         <h1 className="font-bold text-2xl">Hello World</h1>
+        <img
+          src="https://financialportfoliotracker.vercel.app/assets/images/01.jpeg"
+          alt="test"
+        />
       </div>
     </>
   );
