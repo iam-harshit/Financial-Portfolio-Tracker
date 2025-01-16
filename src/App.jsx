40147490d5b95bd68e0d29863717efcfe900e6a9
iam-harshit <importantemail545@gmail.com>
2025-01-16 20:10:15 +0530

    feat: add login component for user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import LoginPage from "./app/login/LoginPage";
+
 const App = () => {
   return (
     <>
-      <div className="flex justify-center items-center">
-        <h1 className="font-bold text-2xl">Hello World</h1>
+      <div>
+        {/* <h1 className="font-bold text-2xl">Hello World</h1>
         <img
           src="https://financialportfoliotracker.vercel.app/assets/images/01.jpeg"
           alt="test"
-        />
+        /> */}
+        <LoginPage />
       </div>
     </>
   );
