3503f8e6d1d9b21174549c6de8505d67b7bf2476
iam-harshit <importantemail545@gmail.com>
2025-01-18 00:27:19 +0530

    feat: handle auth state with useEffect and implement routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import LoginPage from "./app/login/LoginPage";
+import LoginPage from "@/app/login/LoginPage";
+import Home from "@/components/Home";
+import { useEffect } from "react";
+import { auth } from "@/config/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { setupSessionTimeout } from "@/services/auth";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setupSessionTimeout();
+      }
+    });
+  }, []);
+
   return (
     <>
-      <div>
-        {/* <h1 className="font-bold text-2xl">Hello World</h1>
-        <img
-          src="https://financialportfoliotracker.vercel.app/assets/images/01.jpeg"
-          alt="test"
-        /> */}
-        <LoginPage />
-      </div>
+      <Toaster position="top-center" reverseOrder={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
